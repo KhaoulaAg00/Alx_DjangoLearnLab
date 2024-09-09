f1cd7b0534e999cca9224199965b36a4fa81c980
KhaoulaAg00 <khawlaag8@gmail.com>
2024-09-09 23:00:07 +0100

    Delete django_blog directory

--- django_blog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import Register, Login, ProfileView
-urlpatterns = [
-    path('register/', Register.as_view(), name="regiser"),
-    path('login/', Login.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('profile/', ProfileView, name="profile"),
-]
